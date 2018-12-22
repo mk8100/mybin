6630136c931173fd3992c3041182df505955602d
Kapitan Nemo <mk8100@yahoo.com>
2018-12-22 22:13:50 +0100

    cleaning up  to leave only matrix fn

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,10 @@
-use add_two;
-
-use itertools::Itertools;
+use matrix;
 
 fn main() {
-    let res = add_two::run( 3.9);
 
-    println!("Run exit code: {:?}", res);
-
-
-    let m = add_two::Matrix::new(2,3,vec![1,2,3,4,5,6]).unwrap();
-
-    println!("Matrix m = {}",m);
-
-    let v = vec![1,2,3,4,5,6,7,8,9];
-
-    //let l = v.len();
-    let cl  = |(idx, el):(usize, &u32)| {
-                    match idx {
-                        0 => "| ".to_string() + &el.to_string() + ", ", 
-                        i if i == v.len()-1 => el.to_string() + " |", 
-                        _ => el.to_string() + ", "
-                    }
-               };
-
-    let s = v.iter()
-                    .enumerate()
-                    .map( cl)
-                    .collect::<Vec<String>>()
-                    .concat();
-    println!("{}", s);
-
-    let z = v.chunks(3).map(|e| e[0]*e[1]).collect::<Vec<_>>();
-    println!("{:?}", z[0]);
-    let z = v.chunks(3).map(|e| prn(e)).collect::<Vec<_>>().concat();
-    println!("{:?}",z);
-}
-
-
-//fn prn(v:&Vec<u32>) -> String{
-fn prn(v:&[u32]) -> String{
-
-    let cl  = |(idx, el):(usize, &u32)| {
-                    match idx {
-                        0 => "| ".to_string() + &el.to_string() + ", ", 
-                        i if i == v.len()-1 => el.to_string() + " |\n", 
-                        _ => el.to_string() + ", "
-                    }
-               };
-
-    v.iter()
-            .enumerate()
-            .map( cl)
-            .collect::<Vec<String>>()
-            .concat()
+    let m = matrix::Matrix::new(3,3,vec![1,2,3,4,5,6,7,8,9]).unwrap();
+    println!("m:Matrix<i32> = {}",m);
+    
+    let n = matrix::Matrix::new(3,3,vec![1.1,2.2,3.3,4.4,5.5,6.6,7.7,8.8,9.9]).unwrap();
+    println!("n:Matrix<f64> = {}",n);
 }
